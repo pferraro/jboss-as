d764c3bf577e2bd5922c93170e3a497ac68211df
Paul Ferraro <paul.ferraro@redhat.com>
2020-10-02 08:42:02 -0400

    Bump distributable-web subsystem model version

--- clustering/web/extension/src/main/java/org/wildfly/extension/clustering/web/DistributableWebModel.java
+++ clustering/web/extension/src/main/java/org/wildfly/extension/clustering/web/DistributableWebModel.java
@@ -32,9 +32,10 @@ import org.jboss.as.controller.ModelVersion;
 public enum DistributableWebModel implements Model {
 
     VERSION_1_0_0(1, 0, 0), // WildFly 17
-    VERSION_2_0_0(2, 0, 0), // WildFly 18
+    VERSION_2_0_0(2, 0, 0), // WildFly 18-21
+    VERSION_3_0_0(3, 0, 0), // WildFly 22
     ;
-    public static final DistributableWebModel CURRENT = VERSION_2_0_0;
+    public static final DistributableWebModel CURRENT = VERSION_3_0_0;
 
     private final ModelVersion version;
 
